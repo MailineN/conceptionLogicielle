e8e64f70be2bbc524d06681f2eb3e864c3213090
MailineN <nguyen.mailine@gmail.com>
2021-05-10 17:05:13 +0200

    added /cartes

--- utils/functions.py
+++ utils/functions.py
@@ -5,3 +5,16 @@ def getNewDeckID(url):
     json = r.json()
     return(json["deck_id"])
 
+
+def DrawCards(nb_cartes):
+    
+    r = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    deck = r.json()
+    deck_id = deck["deck_id"]
+
+    req = requests.get("https://deckofcardsapi.com/api/deck/"+deck_id+"/draw/?count="+str(nb_cartes))
+    json = req.json()
+    res = {"deck_id": json["deck_id"], "cards": json["cards"]}
+    return(res)
+
+    
